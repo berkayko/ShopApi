de49186b6bf802f8052fe10488dbf0c4a313b709
berkayko <64148740+berkayko@users.noreply.github.com>
2020-12-03 21:39:48 +0000

    Add files via upload

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MmtShopApi.Models
+{
+    public partial class Category
+    {
+        public string Name { get; set; }
+        public int Sku { get; set; }
+    }
+}
---
+++ Models/MMTShopContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace MmtShopApi.Models
+{
+    public partial class MMTShopContext : DbContext
+    {
+        //public MMTShopContext()
+        //{
+        //}
+
+        public MMTShopContext(DbContextOptions<MMTShopContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+////            if (!optionsBuilder.IsConfigured)
+////            {
+////#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+////                //optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=MMTShop;Trusted_Connection=True;");
+////            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Category");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Sku).HasColumnName("SKU");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+
+                entity.Property(e => e.Sku).HasColumnName("SKU");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
